4aa12df38ab02735af9ce6d63571b3117436d8d2
Joshua Mertens <joshua.l.mertens@gmail.com>
2021-02-11 17:37:47 +0000

    minor refactor

--- components/SlideImage.jsx
+++ components/SlideImage.jsx
@@ -2,10 +2,17 @@ import Image from "next/image";
 import imageStyle from "../styles/Slideshow.module.css";
 
 const SlideImage = ({ image, isVisible }) => {
-  const className = isVisible
-    ? imageStyle.image
-    : `${imageStyle.image} ${imageStyle.hidden}`;
-  return <img src={image.src} alt={image.alt} className={className} />;
+  return (
+    <img
+      src={image.src}
+      alt={image.alt}
+      className={
+        isVisible
+          ? imageStyle.image
+          : `${imageStyle.image} ${imageStyle.hidden}`
+      }
+    />
+  );
 };
 
 export default SlideImage;
