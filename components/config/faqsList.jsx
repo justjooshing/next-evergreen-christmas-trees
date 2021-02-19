6a2a057d56c465cdf064aec726813b5d02495093
Joshua Mertens <joshua.l.mertens@gmail.com>
2021-02-19 14:35:06 +0000

    add fqs page

---
+++ components/config/faqsList.jsx
@@ -0,0 +1,58 @@
+export default [
+  {
+    question: "Do you deliver?",
+    answer:
+      "We do not have a delivery service, but we will assist you in putting your tree in or on your car.",
+  },
+  { question: "Do you net your trees?", answer: "Yes, netting is available." },
+  {
+    question: "Can I choose my own tree?",
+    answer:
+      "Yes, we encourage everyone to walk through our trees and choose the tree that will make your Christmas perfect.",
+  },
+  {
+    question: "How often should I water my tree?",
+    answer:
+      "For the first couple of days after you take your tree home, it will drink a few litres of water. Keep a close eye on this. From here, just check the water levels 2-3 times per week to ensure your tree has enough to drink.",
+  },
+  {
+    question: "Do I need to cut down my own tree?",
+    answer:
+      "No, once you have chosen your tree, we will come and cut it down for you and take it to the point of sale for collection.",
+  },
+  {
+    question: "Can I put my tree in direct sunlight?",
+    answer:
+      "It is recommended that you do not leave your tree in direct sunlight. This will cause the tree to go brown sooner.",
+  },
+  {
+    question: "How long will my Christmas tree last?",
+    answer:
+      "If you keep your tree in fresh water and you ensure you regularly top this up, your tree should last for at least 4-6 weeks.",
+  },
+  {
+    question: "Is there anything else to do while at the farm?",
+    answer:
+      "Yes, we have the Enchanted Forest and other activities to explore while you are here.",
+  },
+  {
+    question: "What do I do with my Christmas tree after Christmas",
+    answer:
+      " A lot of councils, such as Kingston, have a service where you are able to leave your tree on your nature strip and book a collection. Please contact your local council to see what is on offer.",
+  },
+  {
+    question: "How do I ensure there are no creepy crawlies in my tree?",
+    answer:
+      "To ensure that no creepy crawlies come inside with you, we recommend that when you get home you spray your tree with bug spray and leave it for a few hours. All creepy crawlies should fall out and your tree will be safe to take inside. We recommend that the tree is kept in water during this time.",
+  },
+  {
+    question: "Do I need to recut the base of my tree once I get it home?",
+    answer:
+      "We advise that you put your tree in water as soon as possible. If this is not possible, the base of the tree will seal over. If this happens you will need to cut 2-3cm off the base of the trunk before putting in water.",
+  },
+  {
+    question: "How do I stand my tree up?",
+    answer:
+      "The best way to stand up your Christmas tree is with a Christmas tree stand. We do sell a selection on site.",
+  },
+];
--- pages/faqs.jsx
+++ pages/faqs.jsx
@@ -1,10 +1,21 @@
 import CustomHead from "../components/Global/CustomHead";
+import faqsList from "../components/config/faqsList";
+
+import faqsStyle from "../styles/FAQs.module.css";
 
 const faqs = () => (
-  <>
+  <div className={faqsStyle.main}>
     <CustomHead pageName="FAQs" />
-    <h1>FAQs</h1>
-  </>
+    <h3>Frequenly Asked Questions</h3>
+    {faqsList.map((faq) => (
+      <details>
+        <summary>
+          <h4>{faq.question}</h4>
+        </summary>
+        {faq.answer}
+      </details>
+    ))}
+  </div>
 );
 
 export default faqs;
