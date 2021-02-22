0959e5fed260ae5043f4f1fef184c0256c0c1653
Joshua Mertens <joshua.l.mertens@gmail.com>
2021-02-22 13:53:25 +0000

    test bug fix on deploy where all images loaded before css could hide them

--- components/Global/SlideImage.jsx
+++ components/Global/SlideImage.jsx
@@ -1,22 +1,32 @@
+import { useState, useEffect } from "react";
+
 import slideStyle from "../../styles/Slideshow.module.css";
 
 const SlideImage = ({ image, isVisible }) => {
+  const [hiddenImages, updateHiddenImages] = useState(null);
+
+  useEffect(() => {
+    updateHiddenImages(
+      <>
+        <img
+          src={image.src}
+          alt={image.alt}
+          className={` ${slideStyle.image} ${slideStyle.hidden}`}
+        />
+        <figcaption className={`${slideStyle.caption} ${slideStyle.hidden}`}>
+          {image.caption}
+        </figcaption>
+      </>
+    );
+  }, [image]);
+
   return isVisible ? (
     <>
       <img src={image.src} alt={image.alt} className={slideStyle.image} />
       <figcaption className={slideStyle.caption}>{image.caption}</figcaption>
     </>
   ) : (
-    <>
-      <img
-        src={image.src}
-        alt={image.alt}
-        className={`${slideStyle.image} ${slideStyle.hidden}`}
-      />
-      <figcaption className={`${slideStyle.caption} ${slideStyle.hidden}`}>
-        {image.caption}
-      </figcaption>
-    </>
+    hiddenImages
   );
 };
 
