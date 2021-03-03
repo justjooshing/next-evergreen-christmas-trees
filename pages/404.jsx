f6ac917a44cba77a92de3a6915494f0017a31adf
Joshua Mertens <joshua.l.mertens@gmail.com>
2021-03-03 10:22:58 +0000

    update lost phrase in 404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -11,7 +11,8 @@ export default function custom404() {
     <>
       <CustomHead pageName={pageName} />
       <div className={custom404Styles.introduction}>
-        <h1>It looks like you're lost</h1>
+        <h2>You're lost!</h2>
+        <h4>You've disappeared in the Enchanted Forest</h4>
         <p>404 - Page not found</p>
         <Image
           src="/404.jpg"
