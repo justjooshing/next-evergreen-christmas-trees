f83d155d83b62bf67a859e3fd3f4e1541fe51b75
Joshua Mertens <joshua.l.mertens@gmail.com>
2021-03-09 12:22:54 +0000

    fix hanging commas

--- components/Global/CustomHead.jsx
+++ components/Global/CustomHead.jsx
@@ -45,48 +45,42 @@ const CustomHead = ({ pageName }) => {
           <meta property="og:locale" content="en_AU" key="oglocale" />
           <script type="application/ld+json">
             {`{
-              "@context": "https://schema.org",
-              "@type": "LocalBusiness",
-              "name": "Evergreen Christmas Trees",
-              "image":
-                "https://evergreenchristmastrees.com/slideshow_images/index/IMG_5249/IMG_5249_200.jpg",
-              "@id": "https://evergreenchristmastrees.com/",
-              "url": "https://evergreenchristmastrees.com/",
-              "telephone": "0400402932",
-              "address": {
-                "@type": "PostalAddress",
-                "streetAddress": "Corner of Thames promenade and Riverend road",
-                "addressLocality": "Bangholme",
-                "addressRegion": "VIC",
-                "postalCode": "3175",
-                "addressCountry": "AU",
-              },
-              "geo": {
-                "@type": "GeoCoordinates",
-                "latitude": -38.042306,
-                "longitude": 145.146012,
-              },
-              "openingHoursSpecification": [
-                {
-                  "@type": "OpeningHoursSpecification",
-                  "dayOfWeek": [
-                    "Monday",
-                    "Tuesday",
-                    "Wednesday",
-                    "Thursday",
-                    "Friday",
-                  ],
-                  "opens": "14:00",
-                  "closes": "18:00",
-                },
-                {
-                  "@type": "OpeningHoursSpecification",
-                  "dayOfWeek": ["Saturday", "Sunday"],
-                  "opens": "09:00",
-                  "closes": "18:00",
-                },
-              ],
-            }`}
+  "@context": "https://schema.org",
+  "@type": "LocalBusiness",
+  "name": "Evergreen Christmas Trees",
+  "image": "https://evergreenchristmastrees.com/slideshow_images/index/IMG_5249/IMG_5249_200.jpg",
+  "@id": "https://evergreenchristmastrees.com/",
+  "url": "https://evergreenchristmastrees.com/",
+  "telephone": "0400402932",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "Corner of Thames promenade and Riverend road",
+    "addressLocality": "Bangholme",
+    "addressRegion": "VIC",
+    "postalCode": "3175",
+    "addressCountry": "AU"
+  },
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": -38.042306,
+    "longitude": 145.146012
+  },
+  "openingHoursSpecification": [
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      "opens": "14:00",
+      "closes": "18:00"
+    },
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": ["Saturday", "Sunday"],
+      "opens": "09:00",
+      "closes": "18:00"
+    }
+  ]
+}
+`}
           </script>
         </>
       )}
