ef590a628909f2069882740dc9bd11baee6e29ee
Joshua Mertens <joshua.l.mertens@gmail.com>
2021-03-09 12:11:00 +0000

    wrap json keys in quotes

--- components/Global/CustomHead.jsx
+++ components/Global/CustomHead.jsx
@@ -47,43 +47,43 @@ const CustomHead = ({ pageName }) => {
             {`{
               "@context": "https://schema.org",
               "@type": "LocalBusiness",
-              name: "Evergreen Christmas Trees",
-              image:
+              "name": "Evergreen Christmas Trees",
+              "image":
                 "https://evergreenchristmastrees.com/slideshow_images/index/IMG_5249/IMG_5249_200.jpg",
               "@id": "https://evergreenchristmastrees.com/",
-              url: "https://evergreenchristmastrees.com/",
-              telephone: "0400402932",
-              address: {
+              "url": "https://evergreenchristmastrees.com/",
+              "telephone": "0400402932",
+              "address": {
                 "@type": "PostalAddress",
-                streetAddress: "Corner of Thames promenade and Riverend road",
-                addressLocality: "Bangholme",
-                addressRegion: "VIC",
-                postalCode: "3175",
-                addressCountry: "AU",
+                "streetAddress": "Corner of Thames promenade and Riverend road",
+                "addressLocality": "Bangholme",
+                "addressRegion": "VIC",
+                "postalCode": "3175",
+                "addressCountry": "AU",
               },
-              geo: {
+              "geo": {
                 "@type": "GeoCoordinates",
-                latitude: -38.042306,
-                longitude: 145.146012,
+                "latitude": -38.042306,
+                "longitude": 145.146012,
               },
-              openingHoursSpecification: [
+              "openingHoursSpecification": [
                 {
                   "@type": "OpeningHoursSpecification",
-                  dayOfWeek: [
+                  "dayOfWeek": [
                     "Monday",
                     "Tuesday",
                     "Wednesday",
                     "Thursday",
                     "Friday",
                   ],
-                  opens: "14:00",
-                  closes: "18:00",
+                  "opens": "14:00",
+                  "closes": "18:00",
                 },
                 {
                   "@type": "OpeningHoursSpecification",
-                  dayOfWeek: ["Saturday", "Sunday"],
-                  opens: "09:00",
-                  closes: "18:00",
+                  "dayOfWeek": ["Saturday", "Sunday"],
+                  "opens": "09:00",
+                  "closes": "18:00",
                 },
               ],
             }`}
