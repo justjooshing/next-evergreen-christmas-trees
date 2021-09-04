35f2475f982398bf7c6205e597d7802e16591547
Josh <joshua.l.mertens@gmail.com>
2021-09-04 10:34:33 +0100

    Start building helper and constants files

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+export const openingHours = [
+    { days: "Monday to Friday", hours: "2pm - 6pm" },
+    { days: "Saturday and Sunday", hours: "9am - 6pm" },
+  ];
+  
---
+++ helpers/index.js
@@ -0,0 +1,8 @@
+const lastFullWeekend = (year, month) => {
+    const date = new Date(year, month);
+    const weekday = date.getDay(); //gets first day of the month
+    const lastSaturday = date.setDate(date.getDate() - weekday - 1); //backtracks to the last Sunday -1 (so, saturday)
+    return date.getDate(); //returns day number
+  };
+
+export const lastSaturday = lastFullWeekend(new Date().getFullYear(), 11);
