035bc5d1db90841c1d81193b2be1e6ceb0b640f4
Josh <joshua.l.mertens@gmail.com>
2021-10-30 13:29:49 +0100

    Move json script into own file to stop Google complaining

--- components/utils/PageWrapper/index.jsx
+++ components/utils/PageWrapper/index.jsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 
 import { metaTags } from "../../../constants";
+import headerData from "../../../constants/headerData";
 
 const CustomHead = ({ pageName }) => {
   const page = metaTags[pageName];
@@ -44,44 +45,7 @@ const CustomHead = ({ pageName }) => {
           />
           <meta property="og:url" content={page.currentURL} key="ogurl" />
           <meta property="og:locale" content="en_AU" key="oglocale" />
-          <script type="application/ld+json">
-            {`{
-              "@context": "https://schema.org",
-              "@type": "LocalBusiness",
-              "name": "Evergreen Christmas Trees",
-              "image": "https://evergreenchristmastrees.com/slideshow_images/index/IMG_5249/IMG_5249_200.jpg",
-              "@id": "https://evergreenchristmastrees.com/",
-              "url": "https://evergreenchristmastrees.com/",
-              "telephone": "0400402932",
-              "address": {
-                "@type": "PostalAddress",
-                "streetAddress": "Corner of Thames promenade and Riverend road",
-                "addressLocality": "Bangholme",
-                "addressRegion": "VIC",
-                "postalCode": "3175",
-                "addressCountry": "AU"
-              },
-              "geo": {
-                "@type": "GeoCoordinates",
-                "latitude": -38.042306,
-                "longitude": 145.146012
-              },
-              "openingHoursSpecification": [
-                {
-                  "@type": "OpeningHoursSpecification",
-                  "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-                  "opens": "14:00",
-                  "closes": "18:00"
-                },
-                {
-                  "@type": "OpeningHoursSpecification",
-                  "dayOfWeek": ["Saturday", "Sunday"],
-                  "opens": "09:00",
-                  "closes": "18:00"
-                }
-              ]
-            }`}
-          </script>
+          <script type="application/json" async src={headerData}></script>
         </>
       )}
     </Head>
