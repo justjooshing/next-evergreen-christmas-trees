83e1baf9c843c630246ed9b5ec83c4dec5f37e4e
Josh <joshua.l.mertens@gmail.com>
2024-09-04 10:45:43 +1000

    add lower bound height to price list items

--- components/atom/PriceList/index.jsx
+++ components/atom/PriceList/index.jsx
@@ -4,15 +4,27 @@ import styles from "./PriceList.module.scss";
 
 const PriceList = ({ prices }) => (
   <ol className={styles.list}>
-    {prices.map(({ height, price }) => (
-      <li className={styles.item} key={`${price}, ${height}`}>
-        <div className={styles.heights}>
-          <span>Up to {height}m:</span>
-          <small className={styles.feet}>({convertToFeet(height)})</small>
-        </div>
-        <span>${price}</span>
-      </li>
-    ))}
+    {prices.map(({ height, price }, i) => {
+      const unitMap = (val, unit) =>
+        ({
+          ft: convertToFeet(val),
+          m: `${val}m`,
+        }[unit]);
+
+      const heightRange = (unit) =>
+        (i === 0 ? "Up" : `${unitMap(height - 0.25, unit)}`) +
+        ` to ${unitMap(height, unit)}`;
+
+      return (
+        <li className={styles.item} key={`${price}, ${height}`}>
+          <div className={styles.heights}>
+            <span>{heightRange("m")}:</span>
+            <small className={styles.feet}>{heightRange("ft")}</small>
+          </div>
+          <span>${price}</span>
+        </li>
+      );
+    })}
   </ol>
 );
 
