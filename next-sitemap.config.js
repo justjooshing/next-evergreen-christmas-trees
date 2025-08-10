26d0ce73d6d0127996b333e468f58e8c0dcae18e
Josh <joshua.l.mertens@gmail.com>
2025-08-10 17:47:26 +1000

    fix: update sitemap config to be module

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+const sitemapConfig = {
   siteUrl: "https://www.evergreenchristmastrees.com",
   generateRobotsTxt: true,
   exclude: ["/admin", "/auth", "/auth/*", "/api", "/api/*"],
 };
+
+export default sitemapConfig;
